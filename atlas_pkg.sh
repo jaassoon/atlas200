f058b2bbdf46f60e04beb2f7fd8d9e982dcbd89a
jaassoon <linglijunmail@gmail.com>
2019-09-29 12:54:21 +0900

    libsnappy

--- atlas_pkg.sh
+++ atlas_pkg.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends libboost-all-dev libatlas-base-dev haveged python-skimage python3-skimage python-pip python3-pip liblmdb-dev libhdf5-serial-dev libsnappy-dev libleveldb-dev  graphviz  libxml2-dev libxml2 libzip-dev libssl-dev sqlite3 python-cycler python-matplotlib python-pil python-pyparsing python-dateutil python-tz python3-cycler python3-matplotlib python3-pil python3-pyparsing python3-dateutil python3-tz qemu-user-static binfmt-support python3-yaml gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+sudo apt-get install -y --no-install-recommends libboost-all-dev libatlas-base-dev haveged python-skimage python3-skimage python-pip python3-pip \
+     liblmdb-dev libhdf5-serial-dev libsnappy-dev libleveldb-dev  graphviz  libxml2-dev libxml2 libzip-dev libssl-dev sqlite3 python-cycler \
+     python-matplotlib python-pil python-pyparsing python-dateutil python-tz python3-cycler python3-matplotlib python3-pil python3-pyparsing \
+     python3-dateutil python3-tz qemu-user-static binfmt-support python3-yaml gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
+sudo apt-get install -y --no-install-recommends libsnappy-dev
 rm -rf /var/lib/apt/lists/*
